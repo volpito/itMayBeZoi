742d8c9304c8ab859592bd476e5141ceee6c5534
Volpito <mpj.rigot@gmail.com>
2021-12-07 14:58:46 +0100

    All green OK but Menu

--- src/Components/HeroBanner/HeroBanner.js
+++ src/Components/HeroBanner/HeroBanner.js
@@ -4,7 +4,7 @@ import banner from '../../Assets/Images/banner.png'
 function HeroBanner() {
   return (
     <main>
-        <div className="bg-red-50">
+        <div className="">
           <div className="inset-x-0 bottom-0 h-1/2 bg-gray-100" />
           <div className="max-w-8xl mx-auto">
             <div className="shadow-xl sm:overflow-hidden">
